a6a9aa2c7a8eea4ea4ef406b6ce5478c6b9837ad
richmondae <richmondakpama09@gmail.com>
2025-06-15 10:19:05 +0100

    Update

--- project/scripts/gallery.js
+++
@@ -1,5 +0,0 @@
-            function onClick(element) {
-                document.getElementById("img01").src = element.src;
-                document.getElementById("modal01").style.display = "block";
-                document.getElementById("caption01").innerHTML = element.alt;
-            }
